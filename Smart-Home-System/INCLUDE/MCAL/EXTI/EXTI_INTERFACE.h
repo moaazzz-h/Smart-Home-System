378bb4bee682b6722820dcab626cde8a581d7c80
moaazzz-h <mezocrafti000@gmail.com>
2026-08-24 12:59:04 +0300

    Add files via upload

---
+++ Smart-Home-System/INCLUDE/MCAL/EXTI/EXTI_INTERFACE.h
@@ -0,0 +1,31 @@
+/*
+ * EXTI_INTERFACE.h
+ *
+ *  Created on: Aug 23, 2026
+ *      Author: HP
+ */
+
+#ifndef INCLUDE_MCAL_EXTI_EXTI_INTERFACE_H_
+#define INCLUDE_MCAL_EXTI_EXTI_INTERFACE_H_
+
+typedef enum {
+	EXTI0 = 0 ,
+	EXTI1,
+	EXTI2
+} EXTI_ID ;
+
+typedef enum {
+	LOW_LEVEL = 0 ,
+	ON_CHANGE,
+	FALLING,
+	RISING
+} EXTI_SENSE_MODE ;
+
+void MEXTI_VoidEnable(EXTI_ID A_EXTI_ID);
+void MEXTI_VoidDisable(EXTI_ID A_EXTI_ID);
+
+void MEXTI_VoidClearFlag(EXTI_ID A_EXTI_ID);
+void MEXTI_VoidConfig(EXTI_ID A_EXTI_ID,EXTI_SENSE_MODE A_EXTI_SENSE_MODE);
+
+void MEXTI_VoidSetCallBack(EXTI_ID A_EXTI_ID, void(*ptrToFunc)(void));
+#endif /* INCLUDE_MCAL_EXTI_EXTI_INTERFACE_H_ */
