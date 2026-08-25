d393304b3264e7de189e01675a7e3c2347673565
moaazzz-h <mezocrafti000@gmail.com>
2026-08-25 12:41:25 +0300

    Refactor ADC initialization and read function

--- SOURCE/ADC_PROGRAM.c
+++ SOURCE/ADC_PROGRAM.c
@@ -7,33 +7,54 @@
 #include "../INCLUDE/LIB/BIT_MATH.h"
 #include "../INCLUDE/LIB/STD_TYPES.h"
 
-// DIO Directives
-
 #include "../INCLUDE/MCAL/ADC/ADC_INTERFACE.h"
 #include "../INCLUDE/MCAL/ADC/ADC_PRIVATE.h"
 #include "../INCLUDE/MCAL/ADC/ADC_CFG.h"
-void MADC_voidInit(void) {
+
+void MADC_voidInit(void)
+{
+    /* Select ADC Reference Voltage */
 
     CLR_BIT(ADMUX_REG, REFS0);
     CLR_BIT(ADMUX_REG, REFS1);
+
     ADMUX_REG |= (ADC_VREF_MODE << REFS0);
 
+    /* Right Adjust Result */
     CLR_BIT(ADMUX_REG, ADLAR);
+
+    /* Select ADC Prescaler */
 
     ADCSRA_REG &= 0xF8;
     ADCSRA_REG |= ADC_PRESCALER;
 
+    /* Enable ADC */
+
     SET_BIT(ADCSRA_REG, ADEN);
 }
 
-u16 ADC_u16ReadSync(unsigned char u8_CopyChannel) {
+
+u16 MADC_u16ReadSync(u8 u8_CopyChannel)
+{
+    u16 Local_u16ADCValue;
+
+    /* Select ADC Channel */
 
     ADMUX_REG &= 0xE0;
-    ADMUX_REG |= (u8_CopyChannel & 0x1F);
+    ADMUX_REG |= (u8_CopyChannel & 0x07);
+
+    /* Start Conversion */
 
     SET_BIT(ADCSRA_REG, ADSC);
 
-    while (GET_BIT(ADCSRA_REG, ADSC) == 1);
+    /* Wait Until Conversion Complete */
 
-    return ADC_DATA;
+    while(GET_BIT(ADCSRA_REG, ADSC) == 1);
+
+    /* Read ADC Result */
+
+    Local_u16ADCValue = ADCL_REG;
+    Local_u16ADCValue |= ((u16)ADCH_REG << 8);
+
+    return Local_u16ADCValue;
 }
